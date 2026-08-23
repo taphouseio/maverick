72fdce61cfdd3e15ecf1e7438d12cf4f73af9a76
Jared Sorge <jared@taphouse.io>
2026-08-23 15:00:04 -0700

    Make snapshot cipher Sendable on Linux

--- Sources/MaverickBroadcast/SnapshotCipher.swift
+++ Sources/MaverickBroadcast/SnapshotCipher.swift
@@ -2,17 +2,18 @@ import Crypto
 import Foundation
 
 struct SnapshotCipher: Sendable {
-    private let key: SymmetricKey
+    private let keyBytes: [UInt8]
 
     init(secret: String) {
         if let decoded = Data(base64Encoded: secret), decoded.count == 32 {
-            key = SymmetricKey(data: decoded)
+            keyBytes = Array(decoded)
         } else {
-            key = SymmetricKey(data: Data(SHA256.hash(data: Data(secret.utf8))))
+            keyBytes = Array(SHA256.hash(data: Data(secret.utf8)))
         }
     }
 
     func encrypt(_ plaintext: Data) throws -> Data {
+        let key = SymmetricKey(data: keyBytes)
         let sealed = try AES.GCM.seal(plaintext, using: key)
         guard let combined = sealed.combined else {
             throw R2StoreError.invalidSnapshot("Unable to create combined encrypted snapshot")
@@ -21,6 +22,7 @@ struct SnapshotCipher: Sendable {
     }
 
     func decrypt(_ encrypted: Data) throws -> Data {
+        let key = SymmetricKey(data: keyBytes)
         let box = try AES.GCM.SealedBox(combined: encrypted)
         return try AES.GCM.open(box, using: key)
     }
